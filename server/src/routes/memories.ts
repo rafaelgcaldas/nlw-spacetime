d74b666c3328f27aabdef2161faa804e093b177c
Rafael <rafael.gcaldas01@gmail.com>
2023-05-22 23:32:41 -0300

    Memories routes

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+   
+  })
+
+  app.get('/memories/:id', async () => {
+   
+  })
+
+  app.post('/memories', async () => {
+    
+  })
+
+  app.put('/memories/:id', async () => {
+    
+  })
+
+  app.delete('/memories/:id', async () => {
+    
+  })
+}
