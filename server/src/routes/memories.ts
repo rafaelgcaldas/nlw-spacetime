8404261cca39e03748d13b0082194240068bbf5a
Rafael <rafael.gcaldas01@gmail.com>
2023-05-22 23:38:56 -0300

    Get memories

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -3,22 +3,26 @@ import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
-   
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 155).concat('...'),
+      }
+    })
   })
 
-  app.get('/memories/:id', async () => {
-   
-  })
+  app.get('/memories/:id', async () => {})
 
-  app.post('/memories', async () => {
-    
-  })
+  app.post('/memories', async () => {})
 
-  app.put('/memories/:id', async () => {
-    
-  })
+  app.put('/memories/:id', async () => {})
 
-  app.delete('/memories/:id', async () => {
-    
-  })
+  app.delete('/memories/:id', async () => {})
 }
