900c8ad38db653eb08afbc025b9ed7399068c4c3
Rafael <rafael.gcaldas01@gmail.com>
2023-06-05 23:16:21 -0300

    Empty memories component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
 
@@ -14,15 +15,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
