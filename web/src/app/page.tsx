7b96879e94c2c9e5d9365ef77e8c40155c56687b
Rafael <rafael.gcaldas01@gmail.com>
2023-06-05 23:13:18 -0300

    SignIn component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,32 +1,15 @@
-import { User } from 'lucide-react'
-
 import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
+        <SignIn />
         <Hero />
-
         <Copyright />
       </div>
 
